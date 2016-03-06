234dc56e3674c72178be59436688451603d7d32b
Michael <mikefaheysd@me.com>
2016-03-05 21:26:04 -0800

    initial commit

---
+++ TextControl/TextControl/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TextControl
+//
+//  Created by Michael Fahey on 3/5/16.
+//  Copyright © 2016 Michael Fahey. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "This is some text" << endl;
+    cout << "Using some more text" << endl;
+    return 0;
+}
